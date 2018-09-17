ee51238ec314e33a271d2a1799ddeb2f73af0c17
Gabe Oleinik <gomusic14@gmail.com>
2018-09-16 21:30:09 -0400

    basic styling, html restructure, minor javascript tweaks

--- public/main.js
+++ public/main.js
@@ -1,108 +1,140 @@
+//sets dealer hand to an empty array
 let dealerHand = []
+
+//sets player hand to an empty array
 let playerHand = []
+
+//sets deck to an empty array
 let deck = []
+
 //where the player score is displayed
 let showPlayer = document.querySelector(".player-score")
+
 //where the dealer score is diplayed
 let showDealer = document.querySelector(".dealer-score")
+
 // where message for game status is diplayed
-let displayStatus =  document.querySelector(".winlose");
+let displayStatus = document.querySelector(".winlose")
+
+//sets up the variables for the scores
 let playerCount = 0
 let dealerCount = 0
 
+//count players score and ends turn after 5 cards
 let countPlayer = () => {
   if (playerHand.length === 2) {
     playerCount = playerHand[0].value + playerHand[1].value
     showPlayer.textContent = playerCount
   }
   if (playerHand.length === 3) {
-    playerCount = playerHand[0].value + playerHand[1].value + playerHand[2].value
+    playerCount =
+      playerHand[0].value + playerHand[1].value + playerHand[2].value
     showPlayer.textContent = playerCount
   }
   if (playerHand.length === 4) {
-    playerCount = playerHand[0].value + playerHand[1].value + playerHand[2].value + playerHand[3].value
+    playerCount =
+      playerHand[0].value +
+      playerHand[1].value +
+      playerHand[2].value +
+      playerHand[3].value
     showPlayer.textContent = playerCount
   }
   if (playerHand.length === 5) {
-    playerCount = playerHand[0].value + playerHand[1].value + playerHand[2].value + playerHand[3].value + playerHand[4].value
+    playerCount =
+      playerHand[0].value +
+      playerHand[1].value +
+      playerHand[2].value +
+      playerHand[3].value +
+      playerHand[4].value
     showPlayer.textContent = playerCount
+    noMore()
   }
-  if (playerHand.length === 6) {
-    playerCount = playerHand[0].value + playerHand[1].value + playerHand[2].value + playerHand[3].value + playerHand[4].value + playerHand[5].value
-    showPlayer.textContent = playerCount
-  }
-
 }
+
+// counts dealers score and ends game after 5 cards
 let countDealer = () => {
   if (dealerHand.length === 2) {
     dealerCount = dealerHand[0].value + dealerHand[1].value
     showDealer.textContent = dealerCount
   }
   if (dealerHand.length === 3) {
-    dealerCount = dealerHand[0].value + dealerHand[1].value + dealerHand[2].value
+    dealerCount =
+      dealerHand[0].value + dealerHand[1].value + dealerHand[2].value
     showDealer.textContent = dealerCount
   }
   if (dealerHand.length === 4) {
-    dealerCount = dealerHand[0].value + dealerHand[1].value + dealerHand[2].value + dealerHand[3].value
+    dealerCount =
+      dealerHand[0].value +
+      dealerHand[1].value +
+      dealerHand[2].value +
+      dealerHand[3].value
     showDealer.textContent = dealerCount
   }
   if (dealerHand.length === 5) {
-    dealerCount = dealerHand[0].value + dealerHand[1].value + dealerHand[2].value + dealerHand[3].value + dealerHand[4].value
+    dealerCount =
+      dealerHand[0].value +
+      dealerHand[1].value +
+      dealerHand[2].value +
+      dealerHand[3].value +
+      dealerHand[4].value
     showDealer.textContent = dealerCount
+    checkWinner()
   }
-  if (dealerHand.length === 6) {
-    dealerCount = dealerHand[0].value + dealerHand[1].value + dealerHand[2].value + dealerHand[3].value + dealerHand[4].value + dealerHand[5].value
-    showDealer.textContent = dealerCount
-  }
-
 }
 
-
+// determines who won
 const checkWinner = () => {
-  console.log("check winner");
-  if (showDealer.textContent > showPlayer.textContent) {
-    displayStatus.textContent = "YOU LOSE.";
-  };
-  if  (showDealer.textContent < showPlayer.textContent) {
-    displayStatus.textContent = "YOU WIN!";
-  };
-  if  (showDealer.textContent === showPlayer.textContent) {
-    displayStatus.textContent = "IT'S A DRAW, THE DEALER WINS.";
-  };
-};
+  console.log("check winner")
+  if (dealerCount > playerCount) {
+    displayStatus.textContent =
+      "DEALER HAS " + `${showDealer.textContent}` + ", YOU LOSE."
+  }
+  if (dealerCount < playerCount) {
+    displayStatus.textContent =
+      "YOU HAVE " + `${showPlayer.textContent}` + ", YOU WIN!"
+  }
+  if (dealerCount === playerCount) {
+    displayStatus.textContent =
+      "YOU BOTH HAVE" + `${showPlayer.textContent}` + ", THE DEALER WINS."
+  }
+}
 
+// arguments for the stay button and ends game if dealer busts
 const noMore = () => {
-  console.log("stay");
+  console.log("stay")
+  // hides the stay and hit button and back of card image in dealers hand.
+  document.querySelector(".hits").classList.add("hideme")
+  document.querySelector(".stays").classList.add("hideme")
+  document.querySelector(".back").classList.add("hideme")
   dealCardToDealer()
   countDealer()
-  // show dealer hidden card and show total then continue down
-  if (showDealer.textContent < 17) {
-    noMore ()
+  if (dealerCount < 17) {
+    noMore()
   }
-  if ((showDealer.textContent >= 17) && (showDealer.textContent <= 21)) {
-    checkWinner();
+  if (dealerCount >= 17 && dealerCount <= 21) {
+    checkWinner()
   }
-  if (showDealer.textContent > 21) {
-    displayStatus.textContent = "DEALER BUSTED, YOU WIN!";
+  if (dealerCount > 21) {
+    displayStatus.textContent =
+      "DEALER BUSTED WITH " + `${showDealer.textContent}` + ", YOU WIN!"
   }
+}
 
-
-};
-
+// argument for the hit button and ends game if player busts
 const giveMore = () => {
-  console.log("hit");
+  console.log("hit")
   dealCardToPlayer()
   countPlayer()
-  if (showPlayer.textContent <= 21) {
-  displayStatus.textContent = "HIT OR STAY"
- };
-  // link to fucntion to initialize players next card
-  if (showPlayer.textContent > 21) {
+  if (playerCount <= 21) {
+    displayStatus.textContent =
+      "YOU HAVE " + `${showPlayer.textContent}` + ", HIT OR STAY"
+  }
+  if (playerCount > 21) {
+    document.querySelector(".hits").classList.add("hideme")
+    document.querySelector(".stays").classList.add("hideme")
     displayStatus.textContent = "YOU BUSTED, PLAY AGAIN"
-
-  };
-};
-
+  }
+}
 
 const dealCardToPlayer = upOrDown => {
   countPlayer()
@@ -113,18 +145,16 @@ const dealCardToPlayer = upOrDown => {
   playerHand.push(card)
 
   // Go find my dealer-hand div
-  const playerHandDiv = document.querySelector('.player-hand')
+  const playerHandDiv = document.querySelector(".player-hand")
 
   // Make a new image tag in memory
-  let image = document.createElement('img')
+  let image = document.createElement("img")
 
   // Tell that image tag where it's image is. We do this dynamically
   // based on the face and the suit
   image.src = `/images/${card.face}${card.suit}.jpg`
-
   // Push that image tag into the DIV as a child
   playerHandDiv.appendChild(image)
-
 }
 
 const dealCardToDealer = upOrDown => {
@@ -135,43 +165,35 @@ const dealCardToDealer = upOrDown => {
   dealerHand.push(card)
 
   // Go find my dealer-hand div
-  const dealerHandDiv = document.querySelector('.dealer-hand')
+  const dealerHandDiv = document.querySelector(".dealer-hand")
 
   // Make a new image tag in memory
-  let image = document.createElement('img')
+  let image = document.createElement("img")
 
   // Tell that image tag where it's image is. We do this dynamically
   // based on the face and the suit
   image.src = `/images/${card.face}${card.suit}.jpg`
 
-  if (upOrDown === 'down') {
-    // Do something to display this card down
-    image.src = `/images/backofcard.jpg`
-    image.className = 'swing'
-  }
-
   // Push that image tag into the DIV as a child
   dealerHandDiv.appendChild(image)
-  // checkWinner()
 }
 
-
 const main = () => {
-  let suits = ['C', 'S', 'D', 'H']
+  let suits = ["C", "S", "D", "H"]
   let cards = [
-    { value: 2, face: '2' },
-    { value: 3, face: '3' },
-    { value: 4, face: '4' },
-    { value: 5, face: '5' },
-    { value: 6, face: '6' },
-    { value: 7, face: '7' },
-    { value: 8, face: '8' },
-    { value: 9, face: '9' },
-    { value: 10, face: '10' },
-    { value: 10, face: 'J' },
-    { value: 10, face: 'Q' },
-    { value: 10, face: 'K' },
-    { value: 11, face: 'A' }
+    { value: 2, face: "2" },
+    { value: 3, face: "3" },
+    { value: 4, face: "4" },
+    { value: 5, face: "5" },
+    { value: 6, face: "6" },
+    { value: 7, face: "7" },
+    { value: 8, face: "8" },
+    { value: 9, face: "9" },
+    { value: 10, face: "10" },
+    { value: 10, face: "J" },
+    { value: 10, face: "Q" },
+    { value: 10, face: "K" },
+    { value: 11, face: "A" }
   ]
 
   // loop through all the suits
@@ -204,14 +226,20 @@ const main = () => {
   }
   dealCardToPlayer("up")
   dealCardToPlayer("up")
-  dealCardToDealer('up')
-  // dealCardToDealer('down')
+  dealCardToDealer("up")
+  //dealCardToDealer('down')
   countPlayer()
-  document.querySelector('.hits').addEventListener('click', giveMore)
- document.querySelector('.stays').addEventListener('click', noMore)
-  document.querySelector('.new-game').addEventListener('click', () => {
-    document.location = '/'
+  // makes the dealer score a ? at the begining
+  showDealer.textContent = "?"
+  // message for begining of game
+  displayStatus.textContent =
+    "YOU HAVE " + `${showPlayer.textContent}` + ", HIT OR STAY"
+  // all the event listener for the buttons
+  document.querySelector(".hits").addEventListener("click", giveMore)
+  document.querySelector(".stays").addEventListener("click", noMore)
+  document.querySelector(".reset").addEventListener("click", () => {
+    document.location = "/"
   })
 }
 
-document.addEventListener('DOMContentLoaded', main)
+document.addEventListener("DOMContentLoaded", main)
